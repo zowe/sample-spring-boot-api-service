cb76d3c59a4b5045997e8d19e63ed02811188be2
Petr Plavjanik <petr.plavjanik@broadcom.com>
2019-05-03 07:08:45 +0200

    start.spring.io generated project

---
+++ src/main/java/org/zowe/sample/apiservice/ZoweApiserviceApplication.java
@@ -0,0 +1,13 @@
+package org.zowe.sample.apiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoweApiserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoweApiserviceApplication.class, args);
+	}
+
+}
