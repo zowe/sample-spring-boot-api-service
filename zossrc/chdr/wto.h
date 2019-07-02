90da17045d8c034f0f1a99985b3dfb5aa2dc10ae
Dan Kelosky <dkelosky@gmail.com>
2019-07-02 07:25:05 -0400

    add license to jno code
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- zossrc/chdr/wto.h
+++ zossrc/chdr/wto.h
@@ -1,6 +1,16 @@
 #ifndef WTO_H                                                             
 #define WTO_H                                                             
-                                                                          
+
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
 #if defined(__IBM_METAL__)                                                
                                                                           
 #define WTO_MODEL(wtom)                                         \         
