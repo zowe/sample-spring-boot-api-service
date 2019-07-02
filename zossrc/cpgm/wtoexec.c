a23fa29a6fde1a21244f849c05a827e3afb6f2a1
Dan Kelosky <dkelosky@gmail.com>
2019-07-02 07:49:17 -0400

    auto format c for vs code linting
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- zossrc/cpgm/wtoexec.c
+++ zossrc/cpgm/wtoexec.c
@@ -7,22 +7,22 @@
  *
  * Copyright Contributors to the Zowe Project.
  */
-#include "wtoexec.h"                                                                  
-#include "wto.h"                                                                      
-                                                                                      
-#include <stdlib.h>                                                                   
-#include <stdio.h>                                                                    
-                                                                                      
+#include "wtoexec.h"
+#include "wto.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
 /**                                                                                   
  * Force the compiler and assembler to treat this function as a                       
  * "main" to aquire stack space.  For production, create your                         
  * own or use your site's PROLOG & EPILOG.                                            
- */                                                                                   
-#pragma prolog(WTOEXE, "&CCN_MAIN SETB 1 \n MYPROLOG")                                
-                                                                                      
-int WTOEXE(int *number, const char *string)                                   
-{                                                                                     
-    WTO_BUF buf = {0};                                                                
+ */
+#pragma prolog(WTOEXE, "&CCN_MAIN SETB 1 \n MYPROLOG")
+
+int WTOEXE(int *number, const char *string)
+{
+    WTO_BUF buf = {0};
     buf.len = sprintf(buf.msg, "Number was: '%d'; String was: '%s'", *number, string);
-    return wto(&buf);                                                                 
-}                                                                                     
+    return wto(&buf);
+}
--- zossrc/cpgm/wtojni.cpp
+++ zossrc/cpgm/wtojni.cpp
@@ -7,57 +7,57 @@
  *
  * Copyright Contributors to the Zowe Project.
  */
-#include <jni.h>                                                                                                                            
-#include <iostream>                                                                                                                         
-#include <string>                                                                                                                           
-#include <unistd.h>                                                                                                                         
-#include "wtojni.h"                                                                                                                         
-#include "wtoexec.h"                                                                                                                        
-                                                                                                                                            
-using namespace std;                                                                                                                        
-                                                                                                                                            
+#include <jni.h>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include "wtojni.h"
+#include "wtoexec.h"
+
+using namespace std;
+
 /**                                                                                                                                         
  * The fields between the pragmas below need to be in ASCII.                                                                                
  *                                                                                                                                          
  * Runtime translations can be done via:                                                                                                    
  *   __etoa() EBCDIC to ASCII                                                                                                               
  *   __atoe() ASCII to EBCDIC                                                                                                               
- */                                                                                                                                         
-#if defined(__IBMC__) || defined(__IBMCPP__)                                                                                                
-#pragma convert(819)                                                                                                                        
-#endif                                                                                                                                      
-                                                                                                                                            
-const char *NATIVE_MESSAGE_FIELD = "nativeMessage";                                                                                         
-const char *FROM_C ="hello world from native code";                                                                                         
-const char *JNI_FIELD_STRING = "Ljava/lang/String;";                                                                                        
-                                                                                                                                            
-#if defined(__IBMC__) || defined(__IBMCPP__)                                                                                                
-#pragma convert(0)                                                                                                                          
-#endif                                                                                                                                      
-                                                                                                                                            
-JNIEXPORT jint JNICALL Java_org_zowe_sample_apiservice_hello_NativeGreeting_wto(JNIEnv *env, jobject thisObj, jint inId, jstring inContent) 
-{                                                                                                                                           
-    int rc = 0;                                                                                                                             
-                                                                                                                                            
-    // input values from Java                                                                                                               
-    const char *content = env->GetStringUTFChars(inContent, NULL);                                                                          
-                                                                                                                                            
-    char contentCopy[100] = {0};                                                                                                            
-                                                                                                                                            
-    // copy, covert, and print input data                                                                                                   
-    strcpy(contentCopy, content);                                                                                                           
-    __atoe(contentCopy);                                                                                                                    
-    cout << "[DEBUG] Input - id: " << (int)inId << " content: " << contentCopy << endl;                                                     
-                                                                                                                                            
-    // call native z/OS service routine                                                                                                     
-    rc = WTOEXE((int *)&inId, contentCopy);                                                                                                 
-                                                                                                                                            
-    // demonstrate setting a String class variable from JNI (simulating returning a value                                                   
-    jclass clazz = env->GetObjectClass(thisObj);                                                                                            
-    jfieldID dataField = env->GetFieldID(clazz, NATIVE_MESSAGE_FIELD, JNI_FIELD_STRING);                                                    
-    jstring cData = env->NewStringUTF(FROM_C);                                                                                              
-    env->SetObjectField(thisObj, dataField, cData);                                                                                         
-                                                                                                                                            
-    // directly return an integer return type                                                                                               
-    return rc;                                                                                                                              
-}                                                                                                                                           
+ */
+#if defined(__IBMC__) || defined(__IBMCPP__)
+#pragma convert(819)
+#endif
+
+const char *NATIVE_MESSAGE_FIELD = "nativeMessage";
+const char *FROM_C = "hello world from native code";
+const char *JNI_FIELD_STRING = "Ljava/lang/String;";
+
+#if defined(__IBMC__) || defined(__IBMCPP__)
+#pragma convert(0)
+#endif
+
+JNIEXPORT jint JNICALL Java_org_zowe_sample_apiservice_hello_NativeGreeting_wto(JNIEnv *env, jobject thisObj, jint inId, jstring inContent)
+{
+    int rc = 0;
+
+    // input values from Java
+    const char *content = env->GetStringUTFChars(inContent, NULL);
+
+    char contentCopy[100] = {0};
+
+    // copy, covert, and print input data
+    strcpy(contentCopy, content);
+    __atoe(contentCopy);
+    cout << "[DEBUG] Input - id: " << (int)inId << " content: " << contentCopy << endl;
+
+    // call native z/OS service routine
+    rc = WTOEXE((int *)&inId, contentCopy);
+
+    // demonstrate setting a String class variable from JNI (simulating returning a value
+    jclass clazz = env->GetObjectClass(thisObj);
+    jfieldID dataField = env->GetFieldID(clazz, NATIVE_MESSAGE_FIELD, JNI_FIELD_STRING);
+    jstring cData = env->NewStringUTF(FROM_C);
+    env->SetObjectField(thisObj, dataField, cData);
+
+    // directly return an integer return type
+    return rc;
+}
