a9df44d4b212bb62485b360794ba01621a09943d
Petr Plavjanik <plavjanik@gmail.com>
2019-09-03 17:12:30 +0200

    Better message on missing configuration
    Signed-off-by: Petr Plavjanik <plavjanik@gmail.com>

--- zowe-api-dev/src/config.ts
+++ zowe-api-dev/src/config.ts
@@ -33,27 +33,30 @@ export interface IProjectConfig {
     zfsMegabytes: number;
 }
 
+const missingProjectConfigHelp = "Change current working directory to a project with zowe-api.json";
+const missingUserConfigHelp = "Use 'zowe-api init' to initialize it";
+
 export function readConfiguration(command: Command, dir: string = "."): [IUserConfig, IProjectConfig] {
     const userConfigPath: string = join(dir, "user-zowe-api.json");
     const projectConfigPath: string = join(dir, "zowe-api.json");
     return [
-        readConfigFile(userConfigPath, command) as IUserConfig,
-        readConfigFile(projectConfigPath, command) as IProjectConfig
+        readConfigFile(userConfigPath, command, missingUserConfigHelp) as IUserConfig,
+        readConfigFile(projectConfigPath, command, missingProjectConfigHelp) as IProjectConfig
     ];
 }
 
 export function readProjectConfiguration(command: Command, dir: string = "."): IProjectConfig {
     const projectConfigPath: string = join(dir, "zowe-api.json");
-    return readConfigFile(projectConfigPath, command) as IProjectConfig;
+    return readConfigFile(projectConfigPath, command, missingProjectConfigHelp) as IProjectConfig;
 }
 
-function readConfigFile(path: string, command: Command): IUserConfig | IProjectConfig {
+function readConfigFile(path: string, command: Command, help: string): IUserConfig | IProjectConfig {
     try {
         return JSON.parse(readFileSync(path, "utf8"));
     } catch (e) {
         const code: string = e.code;
         if (code === "ENOENT") {
-            command.error(`File '${path}' not found. Use 'zowe-api init' to initialize it`);
+            command.error(`File '${path}' not found. ${help}`);
         }
         process.exit(1);
         throw e;
