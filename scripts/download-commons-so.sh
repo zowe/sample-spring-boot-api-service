60df60552156e4164c62bf6f98379fd13daf556b
Petr Plavjanik <plavjanik@gmail.com>
2019-09-03 09:54:55 +0200

    Include .so to the commons JAR
    Signed-off-by: Petr Plavjanik <plavjanik@gmail.com>

---
+++ scripts/download-commons-so.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# TODO: Replace by build on z/OS in CI
+curl https://gist.githubusercontent.com/plavjanik/eb09a5f74844e2d6d305f94a6885f678/raw/8ed5ba65c34e35f5ef389c45995467aee5bc9d9f/libzowe-commons-secur.so | base64 --decode > zowe-rest-api-commons-spring/src/main/resources/lib/libzowe-commons-secur.so
