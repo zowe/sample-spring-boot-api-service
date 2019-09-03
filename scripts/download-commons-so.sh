9a27da878476e20bfd4f9e7174a5ac27d917c0f8
Petr Plavjanik <plavjanik@gmail.com>
2019-09-03 10:15:33 +0200

    fixup!
    Signed-off-by: Petr Plavjanik <plavjanik@gmail.com>

--- scripts/download-commons-so.sh
+++ scripts/download-commons-so.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # TODO: Replace by build on z/OS in CI
+mkdir -p zowe-rest-api-commons-spring/src/main/resources/lib/
 curl https://gist.githubusercontent.com/plavjanik/eb09a5f74844e2d6d305f94a6885f678/raw/8ed5ba65c34e35f5ef389c45995467aee5bc9d9f/libzowe-commons-secur.so | base64 --decode > zowe-rest-api-commons-spring/src/main/resources/lib/libzowe-commons-secur.so
