f3dd0e93cc62baffd5c68e6896cd81884a649a44
Petr Plavjanik <plavjanik@gmail.com>
2019-09-20 21:28:23 +0200

    Cleanup on z/OS
    Signed-off-by: Petr Plavjanik <plavjanik@gmail.com>

--- scripts/zosbuild.sh
+++ scripts/zosbuild.sh
@@ -12,13 +12,25 @@ then
     exit 1
 fi
 
+if [ -z "$ZOS_USERID" ]
+then
+    echo "\$ZOS_USERID is not set. It needs to be set to the user ID that is used for builds"
+    exit 1
+fi
+
+if [ -z "$ZOS_PASSWORD" ]
+then
+    echo "\$ZOS_PASSWORD is not set. It needs to be set to the password of the '$ZOS_USERID' user ID"
+    exit 1
+fi
+
 echo "Preparing Zowe CLI profiles"
 zowe profiles create zosmf-profile $ZOWE_CLI_PROFILE_NAME --host $ZOS_HOST --port $ZOS_ZOSMF_PORT --user $ZOS_USERID --pass "$ZOS_PASSWORD" --reject-unauthorized false --overwrite > /dev/null
-zowe profiles create ssh-profile $ZOWE_CLI_PROFILE_NAME --host river.zowe.org --port $ZOS_SSH_PORT --user $ZOS_USERID --password "$ZOS_PASSWORD" --overwrite > /dev/null
+zowe profiles create ssh-profile $ZOWE_CLI_PROFILE_NAME --host $ZOS_HOST --port $ZOS_SSH_PORT --user $ZOS_USERID --password "$ZOS_PASSWORD" --overwrite > /dev/null
 zowe profiles set-default zosmf $ZOWE_CLI_PROFILE_NAME
 zowe profiles set-default ssh $ZOWE_CLI_PROFILE_NAME
 
-echo "Initializing ZFS"
+echo "Initializing zFS"
 zowe-api-dev init --zosTargetDir $ZOS_TARGET_DIR --zosHlq $ZOS_HLQ --account $ZOS_ACCOUNT_NUMBER --javaHome=$ZOS_JAVA_HOME --force
 zowe-api-dev zfs -p "$ZOS_ZFSADM_PARAM"
 
@@ -33,6 +45,3 @@ cd zowe-rest-api-sample-spring
 zowe-api-dev init --zosTargetDir $ZOS_TARGET_DIR/b$CIRCLE_BUILD_NUM/zowe-rest-api-commons-spring --zosHlq $ZOS_HLQ.B$CIRCLE_BUILD_NUM.COMMONS --account $ZOS_ACCOUNT_NUMBER --javaHome=$ZOS_JAVA_HOME --force
 cd ..
 ./gradlew :zowe-rest-api-sample-spring:zosbuild
-
-echo "Removing $ZOS_TARGET_DIR/b$CIRCLE_BUILD_NUM"
-rm -rf $ZOS_TARGET_DIR/b$CIRCLE_BUILD_NUM
