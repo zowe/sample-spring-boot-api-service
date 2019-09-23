2bc8c6efad4d5b4362fd9a8792b4ba9ce83be6c6
Petr Plavjanik <plavjanik@gmail.com>
2019-09-23 22:46:45 +0200

    fixup!
    Signed-off-by: Petr Plavjanik <plavjanik@gmail.com>

--- scripts/lock-port.sh
+++ scripts/lock-port.sh
@@ -16,7 +16,6 @@ DATA="$CIRCLE_PROJECT_USERNAME:$CIRCLE_PROJECT_REPONAME:$CIRCLE_BUILD_NUM"
 REPEAT=1
 while [ "$REPEAT" -eq "1" ]; do
     curl --include --user $ZOS_USERID:$ZOS_PASSWORD --insecure -X PUT --header 'Content-Type: text/plain' --header 'Accept: application/json' --header 'X-IBM-Data-Type: text' --header 'X-IBM-Migrated-Recall: wait' --header 'X-IBM-Obtain-ENQ: SHRW' -d $DATA "$URL" > lock.out
-    echo "1"
     if grep 'Enqueue for QNAME=SPFEDIT failed' lock.out; then
         echo "Waiting for lock to $DSN_MEMBER to be obtained"
         sleep 5
@@ -31,4 +30,5 @@ while [ "$REPEAT" -eq "1" ]; do
 done
 SESSION_REF=$(grep 'X-IBM-Session-Ref:' lock.out | awk '{ print $2 }')
 echo $SESSION_REF > .lock-session
+echo SESSION_REF=$SESSION_REF
 rm -f lock.out
