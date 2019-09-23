81a1fc1d0cc97153aa2c9374b665c145f849e9e0
Petr Plavjanik <plavjanik@gmail.com>
2019-09-23 11:59:46 +0200

    fixup!
    Signed-off-by: Petr Plavjanik <plavjanik@gmail.com>

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -5,10 +5,11 @@ PID=$!
 echo $PID > .pid
 
 TEST_USERID=zowe2 TEST_PASSWORD=zowe TEST_PORT=10080 TEST_WAIT_MINUTES=1 ./gradlew integrationTest
+TEST_RESULT=$?
 cd ..
 
 PID=$(cat zowe-rest-api-sample-spring/.pid)
 rm zowe-rest-api-sample-spring/.pid
 echo "Killing process $PID"
 kill -9 $PID
-
+exit $TEST_RESULT
