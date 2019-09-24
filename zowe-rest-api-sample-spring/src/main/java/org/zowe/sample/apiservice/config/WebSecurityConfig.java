1e6d6b111dd9a9e5e8e02019005cb4a315d6e1bd
Petr Plavjanik <plavjanik@gmail.com>
2019-09-24 10:28:01 +0200

    Add RestAssuredMockMvc example
    Signed-off-by: Petr Plavjanik <plavjanik@gmail.com>

--- zowe-rest-api-sample-spring/src/main/java/org/zowe/sample/apiservice/config/WebSecurityConfig.java
+++ zowe-rest-api-sample-spring/src/main/java/org/zowe/sample/apiservice/config/WebSecurityConfig.java
@@ -10,7 +10,6 @@
 package org.zowe.sample.apiservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
