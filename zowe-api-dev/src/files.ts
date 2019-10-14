eee0ea9147a800a5cc8b75e45dfdabafdfe6652a
Petr Plavjanik <plavjanik@gmail.com>
2019-10-14 09:26:20 +0200

    Upload only changed files
    Signed-off-by: Petr Plavjanik <plavjanik@gmail.com>

--- zowe-api-dev/src/files.ts
+++ zowe-api-dev/src/files.ts
@@ -11,12 +11,12 @@ import { execSshCommandWithDefaultEnv, execSshCommandWithDefaultEnvCwd, zoweSync
 const debug = Debug("files");
 const userDataDir = ".zowe-api-dev";
 
-function cachedOldFilePath(zosFile: string, profileName: string) {
+export function cachedOldFilePath(zosFile: string, profileName: string) {
     const oldFile = resolve(".", userDataDir, "uploadedFiles", profileName, zosFile.substring(1));
     return oldFile;
 }
 
-function saveFileToOld(file: string, zosFile: string, profileName: string): void {
+export function saveFileToOld(file: string, zosFile: string, profileName: string): void {
     const oldFile = cachedOldFilePath(zosFile, profileName);
     const oldFileDir = dirname(oldFile);
     if (!existsSync(oldFileDir)) {
@@ -33,6 +33,18 @@ function uploadFullFile( command: Command, zosDir: string, file: string, zosFile
     saveFileToOld(file, zosFile, profileName);
 }
 
+export function isFileSame(file: string, zosFile: string, profileName: string): boolean {
+    const oldFile = cachedOldFilePath(zosFile, profileName);
+    debug("Old file: ", oldFile);
+    debug("Current file: ", file);
+    if (existsSync(oldFile)) {
+        const cmp = Buffer.compare(readFileSync(file), readFileSync(oldFile));
+        debug(cmp);
+        return cmp === 0;
+    }
+    return false;
+}
+
 export function transferFiles(
     files: { [filename: string]: ITransferredFile },
     zosTargetDir: string,
@@ -41,6 +53,7 @@ export function transferFiles(
     force: boolean,
     context?: {},
 ) {
+    // tslint:disable-next-line: no-console
     console.time("transferFile");
     checkZoweProfileName(userConfig);
     for (const [file, options] of Object.entries(files)) {
@@ -57,31 +70,26 @@ export function transferFiles(
             execSshCommandWithDefaultEnvCwd(`mkdir -p ${zosDir}`);
             command.log(`Uploading template ${file} to ${zosFile}`);
             zoweSync(`files upload ftu ${tmpPath} ${zosFile}${options.binary ? " --binary" : ""}`);
-        } else if (file.endsWith(".jar")) {
-            const jarpatcherPath = resolve(__dirname, "..", "lib", "jarpatcher.jar");
-            const oldFile = cachedOldFilePath(zosFile, userConfig.zoweProfileName);
-            debug("Old file: ", oldFile);
-            if (!force && existsSync(oldFile)) {
-                if (Buffer.compare(readFileSync(file), readFileSync(oldFile)) === 0) {
-                    command.log(`${file} has not changed`)
-                    return;
-                }
-                else {
-                    command.log(`Patching ${zosFile} to be same as ${file}`);
-                    const patchFile = file + "-patch";
-                    const zosPatchFile = zosFile + "-patch";
-                    const output = execSync(`java -cp ${jarpatcherPath} jarpatcher.JarPatcher diff ${oldFile} ${file} ${patchFile} ${jarpatcherPath}`, { encoding: "utf8" })
-                    debug(output);
-                    zoweSync(`files upload ftu ${patchFile} ${zosPatchFile} --binary`);
-                    execSshCommandWithDefaultEnv(`${userConfig.javaHome}/bin/java  -cp ${zosPatchFile} jarpatcher.JarPatcher patch ${zosFile} ${zosPatchFile} jarpatcher`, zosTargetDir);
-                    saveFileToOld(file, zosFile, userConfig.zoweProfileName);
-                }
+        } else {
+            if (!force && isFileSame(file, zosFile, userConfig.zoweProfileName)) {
+                command.log(`${file} has not changed`)
+                return;
+            }
+            if (file.endsWith(".jar")) {
+                command.log(`Patching ${zosFile} to be same as ${file}`);
+                const patchFile = file + "-patch";
+                const zosPatchFile = zosFile + "-patch";
+                const jarpatcherPath = resolve(__dirname, "..", "lib", "jarpatcher.jar");
+                const oldFile = cachedOldFilePath(zosFile, userConfig.zoweProfileName);
+                const output = execSync(`java -cp ${jarpatcherPath} jarpatcher.JarPatcher diff ${oldFile} ${file} ${patchFile} ${jarpatcherPath}`, { encoding: "utf8" })
+                debug(output);
+                zoweSync(`files upload ftu ${patchFile} ${zosPatchFile} --binary`);
+                execSshCommandWithDefaultEnv(`${userConfig.javaHome}/bin/java  -cp ${zosPatchFile} jarpatcher.JarPatcher patch ${zosFile} ${zosPatchFile} jarpatcher`, zosTargetDir);
+                saveFileToOld(file, zosFile, userConfig.zoweProfileName);
             }
             else {
                 uploadFullFile(command, zosDir, file, zosFile, options, userConfig.zoweProfileName);
             }
-        } else {
-            uploadFullFile(command, zosDir, file, zosFile, options, userConfig.zoweProfileName);
         }
         for (const postCommand of options.postCommands || []) {
             let finalCommand = postCommand;
@@ -92,5 +100,6 @@ export function transferFiles(
             execSshCommandWithDefaultEnv(finalCommand, zosTargetDir);
         }
     }
+    // tslint:disable-next-line: no-console
     console.timeEnd("transferFile");
 }
