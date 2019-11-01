817009f8591cf84c48b135447a0b20fd0ca0da73
Petr Plavjanik <plavjanik@gmail.com>
2019-11-01 13:56:27 +0100

    Use snapshot builds of APIML
    Signed-off-by: Petr Plavjanik <plavjanik@gmail.com>

--- zowe-rest-api-sample-spring/src/main/java/org/zowe/sample/apiservice/greeting/GreetingController.java
+++ zowe-rest-api-sample-spring/src/main/java/org/zowe/sample/apiservice/greeting/GreetingController.java
@@ -13,14 +13,13 @@ import static org.zowe.sample.apiservice.apidoc.ApiDocConstants.DOC_SCHEME_BASIC
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import com.ca.mfaas.rest.response.ApiMessage;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.zowe.commons.rest.response.ApiMessage;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
