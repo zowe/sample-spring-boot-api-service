38bd3495688aedaa8e2aaa6e2208b4d11493b722
MarkAckert <mark.ackert@broadcom.com>
2020-03-04 16:48:01 -0500

    cleanup a few headers missed from build
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- zowe-rest-api-sample-spring/zossrc/wtoexec.h
+++ zowe-rest-api-sample-spring/zossrc/wtoexec.h
@@ -2,12 +2,12 @@
 #define WTOEXEC_H
 
 /*
- * This program and the accompanying materials are made available under the terms of the
- * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-v20.html
- *
- * SPDX-License-Identifier: EPL-2.0
- *
+ * This program and the accompanying materials are made available and may be used, at your option, under either:
+ * * Eclipse Public License v2.0, available at https://www.eclipse.org/legal/epl-v20.html, OR
+ * * Apache License, version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ * 
  * Copyright Contributors to the Zowe Project.
  */
 
