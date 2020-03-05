0b2b54b01cf44c996327f1d629fe1e83a905a671
MarkAckert <mark.ackert@broadcom.com>
2020-03-05 16:43:22 -0500

    fix duplicated license header
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- zowe-rest-api-commons-spring/src/test/java/org/zowe/commons/zos/security/platform/SafPlatformUserTests.java
+++ zowe-rest-api-commons-spring/src/test/java/org/zowe/commons/zos/security/platform/SafPlatformUserTests.java
@@ -8,25 +8,16 @@
  * Copyright Contributors to the Zowe Project.
  */
 
-/*
- * This program and the accompanying materials are made available under the terms of the
- * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-v20.html
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Copyright Contributors to the Zowe Project.
- */
 package org.zowe.commons.zos.security.platform;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.zowe.commons.zos.security.platform.MockPlatformUser.VALID_PASSWORD;
-import static org.zowe.commons.zos.security.platform.MockPlatformUser.VALID_USERID;
 import static org.zowe.commons.zos.security.platform.MockPlatformUser.INVALID_PASSWORD;
 import static org.zowe.commons.zos.security.platform.MockPlatformUser.INVALID_USERID;
+import static org.zowe.commons.zos.security.platform.MockPlatformUser.VALID_PASSWORD;
+import static org.zowe.commons.zos.security.platform.MockPlatformUser.VALID_USERID;
 
 import org.junit.Test;
 
