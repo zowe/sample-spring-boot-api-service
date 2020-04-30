736da221faa12c263ae0891330c9820be5d7915c
JirkaAichler <jiri.aichler@broadcom.com>
2020-04-30 10:19:11 +0200

    Fix Integration tests on zOS
    Signed-off-by: JirkaAichler <jiri.aichler@broadcom.com>

--- scripts/run-integration-tests-kotlin-zos.sh
+++ scripts/run-integration-tests-kotlin-zos.sh
@@ -4,6 +4,7 @@ set -e
 scripts/lock-port.sh
 
 cd zowe-rest-api-sample-kotlin-spring
+zowe-api-dev init --zosTargetDir $ZOS_TARGET_DIR/b$CIRCLE_BUILD_NUM/zowe-rest-api-sample-kotlin-spring --zosHlq $ZOS_HLQ.B$CIRCLE_BUILD_NUM.SAMPLE --account $ZOS_ACCOUNT_NUMBER --javaHome=$ZOS_JAVA_HOME --javaLoadlib $ZOS_JAVA_LOADLIB --force
 zowe-api-dev deploy
 zowe-api-dev config --name zos -p port=$TEST_PORT
 zowe-api-dev start --job --killPrevious
